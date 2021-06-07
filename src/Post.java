467681ed8078e1469c26d9b656aa4f574309b39d
Mirai1412 <kkas0807@naver.com>
2021-06-07 16:03:30 +0900

    JSON

---
+++ src/Post.java
@@ -0,0 +1,46 @@
+
+public class Post {
+	
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	private int userId;
+	private int id;
+	private String title;
+	private String body;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
