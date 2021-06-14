015e79d44eb92489e507d36284b9e2c5767ed6a3
Mirai1412 <kkas0807@naver.com>
2021-06-14 09:23:48 +0900

    Generic

---
+++ src/GenericBox.java
@@ -0,0 +1,13 @@
+public class GenericBox<T> { // < > 제네릭 클래스
+	// <T> : Type parameter
+	private T content;
+
+	public T getContent() {
+		return content;
+	}
+
+	public void setContent(T content) {
+		this.content = content;
+	}
+
+}
---
+++ src/GenericTest.java
@@ -0,0 +1,20 @@
+public class GenericTest {
+
+	public static void main(String[] args) {
+		GenericBox<String> box1 = new GenericBox<>();
+
+		box1.setContent("임채환");
+		String s = box1.getContent();
+		System.out.println(s);
+
+		GenericBox<Integer> box2 = new GenericBox</* Integer */>(); // 뒷부분은 생략가능
+		box2.setContent(100);
+		int n = box2.getContent();
+		System.out.println(n);
+
+		GenericBox<Student> box3 = new GenericBox<Student>();
+		box3.setContent(new Student());
+		Student std = box3.getContent();
+	}
+
+}
