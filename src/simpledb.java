a32b073b0f82caca84741aefdecb3c5d36b07a45
Mirai1412 <kkas0807@naver.com>
2021-06-15 13:17:48 +0900

    데이터 베이스 연동

--- src/simpledb.java
+++ src/simpledb.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -18,139 +19,143 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class simpledb extends JPanel implements ActionListener {
+
+public class simpledb extends JPanel implements ActionListener{
 
 	private JTextField inputField = new JTextField(30);
-	private JButton searchBtn = new JButton("∞Àªˆ");
-	private JButton addBtn = new JButton("√ﬂ∞°");
-	private static final String dirverClassName = "org.mariadb.jdbc.Driver";
-	private static final String DB_SERVER_URL = "jdbc:mysql://localhost:3306/oop";
-	private static final String DB_USER = "root";
-	private static final String DB_USER_PW = "0625";
+	private JButton searchBtn = new JButton("Search");
+	private JButton addBtn = new JButton("Add");
 
-	private Map<String, String> dict = new HashMap<>();
+	Map<String, String> dict = new HashMap<>();
 	private static final String DIC_FILE_NAME = "dict.props";
 
-	public simpledb() {
 
+	// DB
+//	private static final String dirverClassName = "org.mariadb.jdbc.Driver";
+	private static final String DB_SERVER_URL = "jdbc:mysql://localhost:3306/oop";
+	private static final String DB_USER = "root";
+	private static final String DB_USER_PW= "0625";
+	private static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+
+
+	public simpledb() {
 		this.add(inputField);
 		this.add(searchBtn);
-		searchBtn.addActionListener(this);
 		this.add(addBtn);
+
+		searchBtn.addActionListener(this);
 		addBtn.addActionListener(this);
+
 		this.setSize(new Dimension(600, 50));
+
 		buildDictionaryFromDB();
 	}
 
+
+
+
+
+	// DB
 	private void buildDictionaryFromDB() {
 
+		Statement stmt = null;
+		ResultSet rs = null;
+		
 		try {
-			Class.forName(dirverClassName);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			return;
+			Class.forName(JDBC_DRIVER); // Load Driver
+			System.out.println("Connect Success");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Fail to Connect");
+			return; 
 		}
 
-		try (Connection con = DriverManager.getConnection(DB_SERVER_URL, DB_USER, DB_USER_PW);) {
+		try (Connection con = DriverManager.getConnection(DB_SERVER_URL, DB_USER, DB_USER_PW);){ 
 
-			String sql = "select * from dict";
+
+			String sql = "SELECT * FROM dict";
+
 			PreparedStatement pstmt = con.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery(); 
+			while(rs.next()) {
+	
+				String kor = rs.getString("kor");
+	
+				String eng = rs.getString("eng");
+				System.out.println(kor);
 
-			while (rs.next()) {
-
-				String hword = rs.getString("hword");
-				String eword = rs.getString(2);
-				dict.put(hword, eword);
-				dict.put(eword, hword);
+				dict.put(kor, eng);
+				dict.put(eng, kor);
 			}
 
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		}catch(Exception ex) {
+			System.out.println("Load fail" + ex);
 		}
-
-	}
-
-	private void buildDictionaryFromFile() {
-
-		Properties props = new Properties();
-
-		try (FileReader fReader = new FileReader(DIC_FILE_NAME)) {
-			props.load(fReader); // «¡∑Œ∆€∆º ∆ƒ¿œ∞¥√º∏¶ ∑ŒµÂ«‘
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		Set<Object> set = props.keySet();
-		for (Object obj : set) {
-			dict.put((String) obj, (String) props.get(obj));
-		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String key = inputField.getText();
-		if (key.trim().length() == 0)
-			return;
-		if (e.getSource() == addBtn) {
-
-			String value = JOptionPane.showInputDialog(this, key + "ø° ¥Î¿¿µ«¥¬ øµæÓ¥‹æÓ∏¶ ¿‘∑¬");
-			if (value.trim().length() == 0) {
-				return;
-			}
-			dict.put(key, value);
-			dict.put(value, key);
-
-			addWordToFile(key, value);
-			addToDB(key, value);
-			JOptionPane.showMessageDialog(this, "¥‹æÓ √ﬂ∞° øœ∑·.", key, JOptionPane.INFORMATION_MESSAGE);
-		} else if (e.getSource() == searchBtn) {
-
-			System.out.println("[" + key + "]");
-			String value = dict.get(key);
-			if (value != null) {
-
-				JOptionPane.showMessageDialog(this, value, key, JOptionPane.INFORMATION_MESSAGE);
-			} else {
-
-				JOptionPane.showMessageDialog(this, "¥‹æÓ∏¶ √£¿ª ºˆ æ¯Ω¿¥œ¥Ÿ", key, JOptionPane.ERROR_MESSAGE);
-			}
-		}
-
+	
 	}
 
 	private void addToDB(String key, String value) {
+	
+		try (Connection conn = DriverManager.getConnection(DB_SERVER_URL, DB_USER, DB_USER_PW)){
+			String sql = "INSERT INTO dict VALUES(?, ?)"; 
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-		try (Connection con = DriverManager.getConnection(DB_SERVER_URL, DB_USER, DB_USER_PW)) {
-			String sql = "INSERT INTO dict VALUES(? , ?)";
-			PreparedStatement pstmt = con.prepareStatement(sql);
 
 			pstmt.setString(1, key);
 			pstmt.setString(2, value);
 
-			pstmt.executeUpdate();
 
-		} catch (Exception e) {
-			e.printStackTrace();
+			pstmt.executeUpdate(); 
+
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace(); 
 		}
+
 	}
 
-	private void addWordToFile(String key, String value) {
-		try (FileWriter fWriter = new FileWriter(DIC_FILE_NAME, true);) {
-			fWriter.write("\n" + key + "=" + value);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String key = inputField.getText();
+		if(key.trim().length() == 0) return;
+
+		if(e.getSource() == searchBtn) {
+			System.out.println("[" + key + "]");
+			String value = dict.get(key);
+
+			if(value != null) { 
+				JOptionPane.showMessageDialog(this, value, key, JOptionPane.INFORMATION_MESSAGE);
+			}else { 
+				JOptionPane.showMessageDialog(this, "Îã®Ïñ¥Î•º Ï∞æÏùÑ Ïàò ÏóÜÏùå.", key, JOptionPane.ERROR_MESSAGE);
+			}
+
+
+
+		}else if(e.getSource() == addBtn) {
+			String value = JOptionPane.showInputDialog(this,
+					"ÏûÖÎ†•Ìïú "+ key + " Ïóê ÎåÄÏùëÎêòÎäî ÏòÅÏñ¥Îã®Ïñ¥Î•º ÏûÖÎ†•ÌïòÏÑ∏Ïöî.");
+			if(value.trim().length() == 0) return;
+			dict.put(key, value);
+			dict.put(value, key);
+
+			addToDB(key, value);
+			JOptionPane.showMessageDialog(this, value + " ÏòÅÏñ¥Îã®Ïñ¥Í∞Ä Ï∂îÍ∞ÄÎêòÏóàÏäµÎãàÎã§.",
+					key, JOptionPane.INFORMATION_MESSAGE);
 		}
+		inputField.setText("");
+
 	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-
 		SimpleDictionary dictPanel = new SimpleDictionary();
 		frame.add(dictPanel);
-		frame.setTitle("ªÁ¿¸");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Simple Dictionary");
 		frame.pack();
 		frame.setResizable(false);
+		frame.setLocationRelativeTo(dictPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
+
 }
